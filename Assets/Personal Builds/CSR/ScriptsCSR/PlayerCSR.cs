79e768709dc8aa7fa8715ad385e2b11386b92d07
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2021-12-02 12:39:22 +0100

    Player

---
+++ Assets/Personal Builds/CSR/ScriptsCSR/PlayerCSR.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerCSR : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    
+    // Start is called before the first frame update
+    private void Start ()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                print($"Hit {hitInfo.collider.name} ");
+                Move(hitInfo.point);
+            }
+
+        }
+            
+    }
+
+    private void Move(Vector3 point)
+    {
+        Vector3 pint = default;
+        _agent.SetDestination(pint);
+    }
+}
