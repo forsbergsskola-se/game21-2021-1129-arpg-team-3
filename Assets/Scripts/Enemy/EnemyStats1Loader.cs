534ecc88853404f1d03813dd727ee23ad307ef25
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-12-03 15:50:44 +0100

    Enemy healthbar is only visible on mouseover

--- Assets/Scripts/Enemy/EnemyStats1Loader.cs
+++ Assets/Scripts/Enemy/EnemyStats1Loader.cs
@@ -6,5 +6,22 @@ using UnityEngine;
 public class EnemyStats1Loader : MonoBehaviour
 {
     public EnemyStats1 enemyStats1;
-    
+    public EnemyHealthBar enemyHealthBar;
+    private void Update() {
+        ToggleHealthBar();
+    }
+    private void ToggleHealthBar() {
+        if (Physics.Raycast(GetCursorPosition(), out var hitInfo)) {
+            if (hitInfo.collider.CompareTag("Enemy")) {
+                enemyHealthBar.gameObject.SetActive(true);
+            }
+            else {
+                enemyHealthBar.gameObject.SetActive(false);
+            }
+        }
+    }
+    Ray GetCursorPosition() {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Fires ray
+        return ray;
+    }
 }
