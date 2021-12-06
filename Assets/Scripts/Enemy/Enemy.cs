1963ad74d6fbee42f8afc55dd45867663864063e
joechip125 <52743358+joechip125@users.noreply.github.com>
2021-12-06 14:25:35 +0100

    sphere

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 	[SerializeField] protected float health;
 	[SerializeField] protected float maxHealth;
 	[SerializeField] protected float armour;
 	private float damageReceived;
+	[SerializeField] private GameObject sphere;
+	private GameObject useSphere;
 
 	public float Health => health;
 	public float MaxHealth => maxHealth;
@@ -16,6 +19,9 @@ public class Enemy : MonoBehaviour {
 
 	private void Awake() {
 		health = maxHealth;
+		useSphere = Instantiate(sphere);
+		useSphere.transform.position = gameObject.transform.position;
+		useSphere.SetActive(false);
 	}
 	private void LateUpdate() {
 		if (health <= 0) {
@@ -28,7 +34,11 @@ public class Enemy : MonoBehaviour {
 		health -= damageReceived; 
 		health = Mathf.Clamp(health, 0, maxHealth);
 	}
-	private void KillEnemy() {
+	private void KillEnemy()
+	{
+		useSphere.SetActive(true);
+		gameObject.GetComponent<BoxCollider>().isTrigger = true;
+		gameObject.GetComponent<NavMeshObstacle>().enabled = false;
 		gameObject.SetActive(false);
 		Debug.Log("Enemy is Dead");
 	}
