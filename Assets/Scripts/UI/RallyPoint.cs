33ddcf8370cedaa87dc8c0021523dd20fef1c6ca
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-12-06 10:06:32 +0100

    Obtimized Rallypoint

--- Assets/Scripts/UI/RallyPoint.cs
+++ Assets/Scripts/UI/RallyPoint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class RallyPoint : MonoBehaviour
 {
-	private void OnCollisionEnter(Collision other) {
+	private void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Player")) {
 			Destroy(this.gameObject);
 		}
