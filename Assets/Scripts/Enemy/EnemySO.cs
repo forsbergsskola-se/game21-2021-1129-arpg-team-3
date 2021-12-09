db1dab684968290749ccac29bbb0ebc03be0d603
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-12-09 14:19:46 +0100

    Debugging

--- Assets/Scripts/Enemy/EnemySO.cs
+++ Assets/Scripts/Enemy/EnemySO.cs
@@ -6,9 +6,16 @@ public class EnemySO : ScriptableObject {
 	[SerializeField] protected string enemyName;
 	[SerializeField] protected float weaponDamage;
 	[SerializeField] protected float enemyHealth;
+	[SerializeField] protected float enemyArmor;
+	[SerializeField] protected float minDamagePercent;
 
+	
 	public string EnemyName => enemyName;
 	public float WeaponDamage => weaponDamage;
 	public float EnemyHealth => enemyHealth;
+	public float EnemyArmor => enemyArmor;
+	public float MinDamagePercent => minDamagePercent;
+
+
 
 }
