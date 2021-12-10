0f2c72f9766789b28642072260f9b069a34b14c8
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2021-12-10 10:21:14 +0100

    Door opens if you are close and pressing F

--- Assets/Personal Builds/MA/ScriptsMA/DoorMA/OpenDoorMA.cs
+++ Assets/Personal Builds/MA/ScriptsMA/DoorMA/OpenDoorMA.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class OpenDoorMA : MonoBehaviour
 {
-    public GameObject Door;
-
+    public GameObject door;
+    public float minDistance;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && Vector3.Distance(door.transform.position, transform.position) <= minDistance)
         {
             OpenThisDoor();
         }
@@ -17,6 +17,6 @@ public class OpenDoorMA : MonoBehaviour
 
     private void OpenThisDoor()
     {
-        Destroy(Door);
+        Destroy(door);
     }
 }
