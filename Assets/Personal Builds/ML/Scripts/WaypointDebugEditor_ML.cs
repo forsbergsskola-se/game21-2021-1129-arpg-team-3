0eff54c48d55efe0a57654b06ff357948777de09
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2021-12-13 14:09:15 +0100

    Build success!

--- Assets/Personal Builds/ML/Scripts/WaypointDebugEditor_ML.cs
+++ Assets/Personal Builds/ML/Scripts/WaypointDebugEditor_ML.cs
@@ -1,35 +1,35 @@
-//C# Example (LookAtPointEditor.cs)
-using UnityEngine;
-using UnityEditor;
-
-[CustomEditor(typeof(WayPointDebug_ML))]
-//[CanEditMultipleObjects]
-public class WaypointDebugEditor_ML : Editor 
-{
-    SerializedProperty lookAtPoint;
-    
-    void OnEnable()
-    {
-    //    lookAtPoint = serializedObject.FindProperty("lookAtPoint");
-    }
-
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-        
-        WayPointDebug_ML myScript = (WayPointDebug_ML)target;
-        if(GUILayout.Button("Add Node"))
-        {
-            myScript.AddOnePoint();
-        }
-        
-        if(GUILayout.Button("Remove Node"))
-        {
-            myScript.RemoveOnePoint();
-        }
-        
-        //  serializedObject.Update();
-        //  EditorGUILayout.PropertyField(lookAtPoint);
-        //  serializedObject.ApplyModifiedProperties();
-    }
-}
+// //C# Example (LookAtPointEditor.cs)
+// using UnityEngine;
+// using UnityEditor;
+//
+// [CustomEditor(typeof(WayPointDebug_ML))]
+// //[CanEditMultipleObjects]
+// public class WaypointDebugEditor_ML : Editor 
+// {
+//     SerializedProperty lookAtPoint;
+//     
+//     void OnEnable()
+//     {
+//     //    lookAtPoint = serializedObject.FindProperty("lookAtPoint");
+//     }
+//
+//     public override void OnInspectorGUI()
+//     {
+//         DrawDefaultInspector();
+//         
+//         WayPointDebug_ML myScript = (WayPointDebug_ML)target;
+//         if(GUILayout.Button("Add Node"))
+//         {
+//             myScript.AddOnePoint();
+//         }
+//         
+//         if(GUILayout.Button("Remove Node"))
+//         {
+//             myScript.RemoveOnePoint();
+//         }
+//         
+//         //  serializedObject.Update();
+//         //  EditorGUILayout.PropertyField(lookAtPoint);
+//         //  serializedObject.ApplyModifiedProperties();
+//     }
+// }
