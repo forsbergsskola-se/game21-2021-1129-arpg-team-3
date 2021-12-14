7b2ce1323323353a81dc80b3bc7c902158b0e8c0
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-12-14 14:02:38 +0100

    ItemDatabase

--- Assets/ScriptableObjects/Inventory/InventoryScripts/InventoryObjects.cs
+++ Assets/ScriptableObjects/Inventory/InventoryScripts/InventoryObjects.cs
@@ -3,39 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
-public class InventoryObjects : ScriptableObject
+public class InventoryObjects : ScriptableObject, ISerializationCallbackReceiver
 {
+    public ItemDataBaseObject database;
     public List<InventorySlotS> Container = new List<InventorySlotS>();
 
-    public void AddItem(ItemObject _item, int _amount) {
-        bool hasItem = false;
+    public void AddItem(ItemObject _item, int _amount) 
+    {
         for (int i = 0; i < Container.Count; i++)
         {
             if (Container[i].item == _item)
             {
                 Container[i].AddAmount(_amount);
-                hasItem = true;
-                break;
+                return;
             }
         }
-        if (!hasItem)
-        {
-            Container.Add(new InventorySlotS(_item, _amount));
-        }
+        Container.Add(new InventorySlotS(database.GetID[_item], _item, _amount));
     }
-    public void RemoveItem(ItemObject item, int amount) {
+    public void RemoveItem(ItemObject item, int amount) 
+    {
         // Container.Remove(item);
     }
+    public void OnAfterDeserialize()
+    {
+        for (int i = 0; i < Container.Count; i++) 
+            Container[i].item = database.GetItem[Container[i].iD];
+    }
+    public void OnBeforeSerialize()
+    {
+    }
+    
 }
 
 [System.Serializable]
 public class InventorySlotS
 {
+    public int iD;
     public ItemObject item;
     public int amount;
 
-    public InventorySlotS(ItemObject _item, int _amount)
+    public InventorySlotS(int _iD, ItemObject _item, int _amount)
     {
+        iD = _iD;
         item = _item;
         amount = _amount;
     }
