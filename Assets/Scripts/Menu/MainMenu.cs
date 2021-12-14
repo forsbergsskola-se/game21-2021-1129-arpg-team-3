e127fae0bb71fe97179dcd9a125060b6c3e48de1
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-12-14 17:25:31 +0100

    Jazzed up the Main Menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public InventoryObjects inventory;
+	public void NewGame() 
+	{
+		SceneManager.LoadScene(1);
+	}
+	public void LoadGame()
+	{
+		SceneManager.LoadScene(1);
+		inventory.Load();
+	}
+	public void ExitGame()
+	{
+		Debug.Log("I'm out, beaches!");
+		Application.Quit();
+	}
+}
