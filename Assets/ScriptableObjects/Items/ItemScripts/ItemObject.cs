cd51acfdb46a116375ec1fea7e7d0049601fa0ed
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-12-15 15:53:09 +0100

    ITembuffs

--- Assets/ScriptableObjects/Items/ItemScripts/ItemObject.cs
+++ Assets/ScriptableObjects/Items/ItemScripts/ItemObject.cs
@@ -9,6 +9,17 @@ public enum ItemTypeS
     KeyItem
 }
 
+public enum Attributes
+{
+    AttackSpeed,
+    DamageBonus,
+    HPBonus,
+    DefenseBonus,
+    AppearanceBonus,
+    Intelligence,
+    Stamina
+    
+}
 public abstract class ItemObject : ScriptableObject
 {
     public int ID;
@@ -16,6 +27,13 @@ public abstract class ItemObject : ScriptableObject
     public ItemTypeS type;
     [TextArea(15,20)]
     public string description;
+    public ItemBuff[] buffs;
+
+    public Item CreateItem()
+    {
+        Item newItem = new Item(this);
+        return newItem;
+    }
 }
 
 [System.Serializable]
@@ -23,10 +41,35 @@ public class Item
 {
     public string Name;
     public int Id;
-
+    public ItemBuff[] buffs;
     public Item(ItemObject item)
     {
         Name = item.name;
         Id = item.ID;
+        buffs = new ItemBuff[item.buffs.Length];
+        for (int i = 0; i < buffs.Length; i++)
+        {
+            buffs[i] = new ItemBuff(item.buffs[i].min, item.buffs[i].max);
+        }
+    }
+}
+[System.Serializable]
+public class ItemBuff
+{
+    public Attributes attribute;
+    public int value;
+    public int min;
+    public int max;
+
+    public ItemBuff(int _min, int _max)
+    {
+        min = _min;
+        max = _max;
+        GenerateValue();
+    }
+
+    public void GenerateValue()
+    {
+        value = UnityEngine.Random.Range(min, max);
     }
 }
