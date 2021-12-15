e65a2020cd2e7bcba2f07518d7f249bb98f84d12
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2021-12-15 11:38:40 +0100

    Update IDamageableYY.cs

--- Assets/Personal Builds/YY/ScriptsYY/MeshScriptsYY/IDamageableYY.cs
+++ Assets/Personal Builds/YY/ScriptsYY/MeshScriptsYY/IDamageableYY.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IDamageableYY : MonoBehaviour
+public interface IDamageableYY 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void TakeDamage(int Damage);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Transform GetTransform();
 }
