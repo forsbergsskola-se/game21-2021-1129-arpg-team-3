91617b3bdb818af64ec64c80fd4a0de366094d44
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2021-12-20 11:10:55 +0100

    minimap copied to work with

---
+++ Assets/Personal Builds/YY/ScriptsYY/Camera, MinimapYY/MinimapYY.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapYY : MonoBehaviour
+{
+    public Transform miniMapCameraTransform;
+    public Transform playerTransform;
+    private Vector3 cameraFromPlayerOffset;
+	
+    void Start()
+    {
+        cameraFromPlayerOffset = new Vector3(0, 11, 0);
+    }
+    private void LateUpdate() {
+        miniMapCameraTransform.position = playerTransform.position + cameraFromPlayerOffset;
+    }
+}
