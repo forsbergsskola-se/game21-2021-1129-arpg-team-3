e1a111fda6644fac23a07ece896d0c78f4514500
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-01-06 11:00:16 +0100

    update

--- Assets/ScriptableObjects/Inventory/InventoryScripts/TradeInterface.cs
+++ Assets/ScriptableObjects/Inventory/InventoryScripts/TradeInterface.cs
@@ -28,6 +28,10 @@ public class TradeInterface : UserInterface
     private readonly int X_SPACE_BETWEEN_ITEM = 65;
     private readonly int NUMBER_OF_COLUMN = 4;
     private readonly int Y_SPACE_BETWEEN_ITEM = 50;
+
+    public delegate void MakeSaleDelegate(GameObject obj, int slotIndex);
+
+    public static event MakeSaleDelegate OnMakeSale;
     
     public override void CreateSlots()
     {
@@ -36,57 +40,62 @@ public class TradeInterface : UserInterface
         slotsOnInterface = new Dictionary<GameObject, InventorySlotS>();
         for (int i = 0; i < inventory.GetSlots.Length; i++)
         {
+            int index = i;
             var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             AddEvent(obj, EventTriggerType.PointerEnter,delegate{OnEnter(obj);});
             AddEvent(obj, EventTriggerType.PointerExit,delegate{OnExit(obj);});
-            AddEvent(obj, EventTriggerType.PointerClick, delegate{OnPointerClick(obj);});
+            AddEvent(obj, EventTriggerType.PointerClick, delegate{OnPointerClick(obj, index);});
             inventory.GetSlots[i].slotDisplay = obj;
             slotsOnInterface.Add(obj, inventory.GetSlots[i]);
         }
 
-        DoAThing(0);
+        SetupSlots();
     }
 
-    private void GetLowestIndex()
+    private void SetupSlots()
     {
-        
-    }
-    
-    private void DoAThing(int index)
-    {
-        bool lowest = false;
-        inventory.GetSlots[index].slotDisplay.transform.GetChild(0).GetComponentInChildren<Image>().sprite = itemToAdd[index].uiDisplay;
-        inventory.GetSlots[index].slotDisplay.GetComponentInChildren<TextMeshProUGUI>().text = items[index].amount.ToString();
-        inventory.GetSlots[index].slotDisplay.transform.GetChild(0).GetComponentInChildren<Image>().color = new Color(1, 1, 1, 1);
+        for (int i = 0; i < inventory.GetSlots.Length; i++)
+        {
+            inventory.GetSlots[i].slotDisplay.transform.GetChild(0).GetComponentInChildren<Image>().sprite =
+                itemToAdd[i].uiDisplay;
+            inventory.GetSlots[i].slotDisplay.GetComponentInChildren<TextMeshProUGUI>().text =
+                items[i].amount.ToString();
+            inventory.GetSlots[i].slotDisplay.transform.GetChild(0).GetComponentInChildren<Image>().color =
+                new Color(1, 1, 1, 1);
+        }
     }
     
     private void SetupButtons()
     {
-        GameObject.FindWithTag("BuyButton").GetComponent<Button>().interactable = false;
+        GameObject.FindWithTag("BuyButton").GetComponentInChildren<Button>().interactable = false;
     }
 
-    private void OnPointerClick(GameObject obj)
+    private void OnPointerClick(GameObject obj, int slotIndex)
     {
-        var test = inventory.GetSlots
+        inventory.GetSlots
             .Where(x => x.slotDisplay.transform.position != obj.transform.position).ToList()
             .Select(x => x.slotDisplay.transform.GetComponentsInChildren<Image>()[0].color = new Color(1, 1, 1, 1)).ToList();
 
         obj.transform.GetComponentsInChildren<Image>()[0].color = new Color(0, 1, 0, 1);
-        var buyButton = GameObject.FindWithTag("BuyButton").GetComponent<Button>();
+
+        GameObject.FindWithTag("BuyButton").GetComponentsInChildren<TextMeshProUGUI>()[1].text = "Cost: "  + itemToAdd[slotIndex].baseValue;
+        var buyButton = GameObject.FindWithTag("BuyButton").GetComponentInChildren<Button>();
+        buyButton.onClick.RemoveAllListeners();
         buyButton.interactable = true;
-        buyButton.onClick.AddListener(() => BuyButtonClick(obj));
+        buyButton.onClick.AddListener(() => BuyButtonClick(obj, slotIndex));
     }
 
-    private void BuyButtonClick(GameObject obj)
+    private void BuyButtonClick(GameObject obj, int index)
     {
-        inventory.AddItem(itemToAdd[0].data, 1);
-
-        foreach (var el in inventory.GetSlots)
+        if (items[index].amount > 0)
         {
-            Debug.Log(el.amount);
+            items[index].amount--;
+            obj.transform.GetComponentInChildren<TextMeshProUGUI>().text = items[index].amount.ToString();
         }
     }
+    
+    
     
     private Vector3 GetPosition(int i)
     {
