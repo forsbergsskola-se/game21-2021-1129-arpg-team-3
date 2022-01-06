96181399761989e0f52e7e7982cd7db4c5e7d4ac
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-06 11:52:01 +0100

    Player Replenishes Health when reaching a checkpoint

--- Assets/Scripts/UI/Checkpoint.cs
+++ Assets/Scripts/UI/Checkpoint.cs
@@ -2,33 +2,37 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour {
+public class Checkpoint : MonoBehaviour 
+{
 	private bool triggered;
 	public TextMeshProUGUI text;
 	// public GameObject effect;
 
-	private void Awake() {
+	private void Awake() 
+	{
 		triggered = false;
 	}
 
-	private void OnTriggerEnter(Collider other) {
+	private void OnTriggerEnter(Collider other) 
+	{
 		if (triggered)
 			return;
 		if (!other.gameObject.CompareTag("Player"))
 			return;
 		Trigger();
 		other.gameObject.GetComponent<SpawnControl>().spawnPosition = transform.position;
+		other.gameObject.GetComponent<SpawnControl>().ReplenishHealth();
 		FMODUnity.RuntimeManager.PlayOneShot("event:/Player/PlayerCheckpoint");
-		// effect.GetComponent<ParticleSystem>().Play();
 		StartCoroutine(CheckpointText());
 	}
-
-	private void Trigger() {
+	private void Trigger() 
+	{
 		triggered = true;
 	}
 
-	private IEnumerator CheckpointText() {
-		text.text = "CHECKPOINT";
+	private IEnumerator CheckpointText() 
+	{
+		text.text = "CHECKPOINT! Health Replenished!";
 		yield return new WaitForSeconds(3);
 		text.text = "";
 	}
