abfcb9f3a2564c96c6e44edaa1d7fb952cd35652
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-10 12:21:02 +0100

    Attack Spark and Blood feedback

--- Assets/Scripts/Player/PlayerDamage.cs
+++ Assets/Scripts/Player/PlayerDamage.cs
@@ -7,6 +7,7 @@ public class PlayerDamage : MonoBehaviour
 
 	private PlayerStats playerStats;
 	public GameObject damageText;
+	public GameObject sparks;
 
 	[SerializeField] private float timeToTakeDamage;
 	private float elapsedTime;
@@ -16,8 +17,10 @@ public class PlayerDamage : MonoBehaviour
 	{
 		playerStats = GetComponent<PlayerStatsLoader>().playerStats;
 	}
-	private void OnCollisionEnter(Collision other) {
-		if (other.gameObject.CompareTag("SSword")) {
+	private void OnCollisionEnter(Collision other) 
+	{
+		if (other.gameObject.CompareTag("SSword")) 
+		{
 			FMODUnity.RuntimeManager.PlayOneShot("event:/Player/SwordHit");
 			float damageReceived = (other.gameObject.GetComponentInParent<Enemy>().enemySo.WeaponDamage + playerStats.PlayerLevel * 5 * 0.75f)  * Random.Range(0.9f, 1f) - playerStats.PlayerArmour;
 			damageReceived = Mathf.Clamp(damageReceived, 0, 10000);
@@ -25,14 +28,17 @@ public class PlayerDamage : MonoBehaviour
 			ShowPlayerDamage(damageReceived);
 		}
 	}
-	private void OnParticleCollision(GameObject other) {
+	private void OnParticleCollision(GameObject other) 
+	{
 		float damageReceived = (20 + playerStats.PlayerLevel * 5 * playerStats.Vulnerability) * Random.Range(0.9f, 1f) - playerStats.PlayerArmour;
 		damageReceived = Mathf.Clamp(damageReceived, 0, 10000);
 		playerStats.TakeDamage(damageReceived);
-		ShowPlayerDamage(damageReceived);	
+		ShowPlayerDamage(damageReceived);
 	}
+	
 	private void ShowPlayerDamage(float damageReceived)
 	{
+		Instantiate(sparks, transform.position, Quaternion.identity);
 		FMODUnity.RuntimeManager.PlayOneShot("event:/Player/PlayerHurt");
 		DamageIndicator indicator = Instantiate(damageText, transform.position, Quaternion.identity).GetComponent<DamageIndicator>();
 		indicator.SetDamageText(Convert.ToInt32(damageReceived));
