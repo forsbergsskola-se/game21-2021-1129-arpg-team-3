c4563303bd6d40371b18e96629b17d9da0d6f12b
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2022-01-11 10:56:35 +0100

    Update RallyPoint.cs

--- Assets/Scripts/UI/RallyPoint.cs
+++ Assets/Scripts/UI/RallyPoint.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class RallyPoint : MonoBehaviour
 {
-	private void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag("Player")) {
+	private void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Player")) 
+		{
 			Destroy(this.gameObject);
 		}
 	}
