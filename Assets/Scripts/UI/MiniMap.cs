6033e766607561d57f582ec0580ee9c053f21af7
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2022-01-11 10:52:28 +0100

    Update MiniMap.cs

--- Assets/Scripts/UI/MiniMap.cs
+++ Assets/Scripts/UI/MiniMap.cs
@@ -14,12 +14,14 @@ public class MiniMap : MonoBehaviour
 	{
 		cameraFromPlayerOffset = new Vector3(0, 11, 0);
 	}
-	private void LateUpdate() {
+	private void LateUpdate() 
+	{
 		miniMapCameraTransform.position = playerTransform.position + cameraFromPlayerOffset;
 		var position = player.position;
 		transform.RotateAround(position, player.up, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed); 
 		
-		if (Input.GetMouseButton(2)) {
+		if (Input.GetMouseButton(2)) 
+		{
 			Vector3 direction = previousPosition - cam.ScreenToViewportPoint(Input.mousePosition);
 			cam.transform.position = player.position;
 			cam.transform.Rotate(new Vector3(0,0.1f,0),-direction.x*90,Space.World);
