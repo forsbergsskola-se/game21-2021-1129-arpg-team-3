e93ec8813c42cfe215a29c878b9bf494b56922a9
Yasin Yekta <yasin.cakiroglu@student.forsbergsskola.se>
2022-01-11 10:52:50 +0100

    Update LevelNotification.cs

--- Assets/Scripts/UI/LevelNotification.cs
+++ Assets/Scripts/UI/LevelNotification.cs
@@ -9,14 +9,16 @@ public class LevelNotification : MonoBehaviour
 	private bool triggered;
 	public int chapter;
 
-	private void OnTriggerEnter(Collider other) {
+	private void OnTriggerEnter(Collider other) 
+	{
 		if (other.gameObject.CompareTag("Player") && !triggered) {
 			FMODUnity.RuntimeManager.PlayOneShot("event:/Player/PlayerRespawn");
 			StartCoroutine(MessageText());
 			triggered = true;
 		}
 	}
-	private IEnumerator MessageText() {
+	private IEnumerator MessageText() 
+	{
 		text.text = $"Chapter {chapter} Complete!";
 		yield return new WaitForSeconds(3);
 		text.text = "";
